b26c33cb559a49aedaca984ed83584b84d3bd47c
Majd Rezik <majdrezik@gmail.com>
2022-06-17 02:26:25 +0300

    init

---
+++ Backend/Frontend/src/components/LoadUserPage/index.jsx
@@ -0,0 +1,67 @@
+import React, { Component, useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { logged, setLogged } from "../../redux/Global";
+import { currentUser, setCurrentUser } from "../../redux/Global";
+import { useHistory } from "react-router-dom";
+import axios from 'axios';
+import { axiosInstance } from '../../config.js';
+
+
+
+export default function DashboardPage({ match }) {
+    const [em,setEm]=useState("")
+    const isLogged = useSelector(logged);
+    const currentuser = useSelector(currentUser);
+    const dispatch = useDispatch();
+
+    let history = useHistory();
+    const rememberme = localStorage.getItem('remeberme');
+    if(rememberme ==='true'){
+        const email = localStorage.getItem('email');
+        const password = localStorage.getItem('password');
+        setEm(email)
+        axiosInstance.post('/users/getuser',{email: email, password: password, recaptcha:"1"})
+        .then(response => {
+        var res= JSON.parse(JSON.stringify(response.data));
+
+        if(res.Email!=="" && res.Email === email.toLowerCase()){
+            dispatch(setLogged(1));
+            dispatch(setCurrentUser({
+                Name: res.Name,
+                FamilyName: res.FamilyName,
+                Email: res.Email,
+                PromoCode: res.PromoCode,
+                PhoneNumber: res.PhoneNumber,
+                PhoneNumber: res.PhoneNumber,
+                Country: res.Country,
+                City: res.City,
+                Street: res.Street,
+                ZipCode: res.ZipCode
+            }));
+            window.location.href = 'Dashboard'
+        }
+        else   
+        window.location.href = 'Dashboard'
+
+        
+        })
+    .catch((error) => {
+    console.log(error);
+    window.location.href = 'Dashboard'
+
+    })
+
+    }
+    else
+    window.location.href = 'Dashboard'
+
+
+        
+    
+    return (
+   <div>
+       <h1>Loading... {em}</h1>
+   </div>
+        );
+}
+
---
+++ Backend/Frontend/src/components/UpdateEmailPage/index.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { axiosInstance } from '../../config.js';
+
+
+export default class updateEmailPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            status:""
+        }
+    }
+
+    activateButton = ()=>{
+        if(window.location.href.split('?').length<2){
+            this.setState({status:"error"})
+        }else{
+        axiosInstance.get('/users/confirmemail?'+window.location.href.split('?')[1])
+            .then((response) => {
+                this.setState({status:response.data})
+                if(response.data==="Your Email Has been Updated successfully!!")
+                    window.location.href = 'logout'
+            })
+            .catch((error) => {
+                this.setState({status:"error: " +error})
+                console.log(error);
+            })
+    }
+    }
+
+      render() {
+  return (
+    <div class="container" style={{color:"#ffffff"}}>
+    <button type="button"  onClick={this.activateButton}> Click Here to Confirm Email</button>
+        <h1>{this.state.status}</h1>
+
+            <Link to="/sign-in">
+            <button >
+            Go to Login Page
+            </button>
+            </Link>
+
+    </div>
+     );
+};
+}
